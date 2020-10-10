10578664f5c95f2f752587aa6cd01634efc669e3
Owais <owais.a.n7@gmail.com>
2020-10-10 14:05:26 -0400

    numsort

--- practice2.cpp
+++ practice2.cpp
@@ -4,54 +4,72 @@
 
 using namespace std;
 
-//declare function
+int sortNums(int nums[], int sizes);
+int swapper(int& a, int& b);
 int genRandom(int max); //random number generator
-int game(int max, int round); // function that contains the game
 
+int main() { 
+    int size = 5;
+    int num[size];
+    
+    
+    for (int i = 0; i < size; i++)
+    {
+        int randomNum = genRandom(100);
+        num[i] = randomNum;
+        cout << num[i] << endl;
+    }
+    
 
-int main () {
-    game(9,1);
+    //array before function
+    cout << "before:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << num[i] << ", ";
+    }
+    cout  << endl;
 
+    sortNums(num, size);
+  
+    //after function
+    cout << "after: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << num[i] << ", ";
+    }
+    cout  << endl;
+  
+
+    return 0;
 }
 
-//define function
+int sortNums(int nums[], int sizes) {
+    //function
+  for (int i = 0; i < sizes; i++)
+  {
+      for (int j = 0; j < sizes; j++)
+      {
+          if (nums[j] > nums[i])
+          {
+              swapper(nums[j], nums[i]);
+          }
+          
+      }
+      
+  }
+  return 0;
+}
+
+int swapper(int& a, int& b) {
+  int z;
+  z = a;
+  a = b;
+  b = z;
+
+  return 0;
+}
+
 int genRandom(int max) {
     srand(time(NULL));
     return (rand()%max + 1);
-}
-
-int game(int max, int round) {
-        cout <<"ROUND " << round << endl;
-        int guess;
-        int rand = genRandom(max);
-        int x = 0;
-        char play_again = 'Y';
-        while (x < 5){
-            cout << "Guess a location of the battleship [0," << max << "]: " << endl;
-            
-            cin >> guess;
-            if (rand != guess) {
-                x += 1;
-                cout << "MISS!" << endl;
-            }
-            else {
-                x = 5;
-                cout << "HIT!" << endl;
-            } 
-            cout << " " << endl;
-        }
-        cout << "PLAY AGAIN! (Y/N) " << endl;
-        cin >> play_again;
-
-        switch (play_again){
-        case 'N':
-            break;
-        
-        case 'Y':
-            game(max + 10, round + 1);
-            break;
-        default:
-            break;
-        } 
-    return 0;
 }
